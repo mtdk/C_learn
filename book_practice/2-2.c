6b72761befaa5e8b5f225d7b575185c226c35e9d
QINGHE-XRX <ruixiangxie@hotmail.com>
2022-05-05 14:39:33 +0800

    202205051439

--- book_practice/2-2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	printf("Programming is fun. \n");
-	printf("And programming in C is even more fun!\n");
-	
-	return 0;
-}
--- ming_ri/3-4-3-3.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(){
-	printf("一切皆有可能！\n");
-	printf("Nothing is impossible!\n");
-}
