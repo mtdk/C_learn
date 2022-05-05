ee2a670bc9b5026315ea52f20c5a44b841d5d72f
QINGHE-XRX <ruixiangxie@hotmail.com>
2022-05-05 17:58:47 +0800

    "202205051758"

---
+++ code_1/6-6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+
+				char c[15]={'I',' ','a','m',' ','a',' ','s','t','u','d','e','n','t','.'};
+				
+				int i;
+				for(i=0;i<15;i++){
+					printf("%c",c[i]);
+				}
+				printf("\n");
+
+				return 0;
+}
---
+++ code_1/6-7.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+
+				char diamond[][5]={{' ',' ','*'},{' ','*',' ','*',},{'*',' ',' ',' ','*'},{' ','*',' ','*'},{' ',' ','*'}};
+				int i,j;
+				for(i=0;i<5;i++){
+					for(j=0;j<5;j++){
+						printf("%c",diamond[i][j]);
+					}
+					printf("\n");
+				}
+				return 0;
+}
